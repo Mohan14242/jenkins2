919bf770fe3a66f9742e90f4cfa7585f247fefcb
mohan <mohan14242@gmail.com>
2024-01-04 19:45:39 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "the name os the shell script is $0"
-echo "the nummer of command line arguments are passed $#"
+echo "script name is $0"
 
 
 
-if [ $# -ne 0 ] then 
-    echo "
+
+
