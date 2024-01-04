2a7899702660921aaad0ebc7331f8a964daa91f5
mohan <mohan14242@gmail.com>
2024-01-04 15:56:44 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-my_array=("mohan" "chiru" "vijay")
-echo "first fruit is ${my_array[0]}"
-echo "second_fruit is ${my_array[1]}"
+echo "the name os the shell script is $0"
+echo "the nummer of command line arguments are passed $#"
