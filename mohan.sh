608b179eab2221e75fcd63b6843c581f2d332c88
mohan <mohan14242@gmail.com>
2024-01-04 15:51:31 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 my_array=("mohan" "chiru" "vijay")
-echo "first fruit is $my_array[0]"
-echo "second_fruit is $my_array[1]"
+echo "first fruit is ${my_array[0]}"
+echo "second_fruit is ${my_array[1]}"
