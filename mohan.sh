60b7c0eb937228a0d6df5b4ccb4e67152e7255d0
mohan <mohan14242@gmail.com>
2024-01-04 16:59:11 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -3,8 +3,7 @@
 echo "the name os the shell script is $0"
 echo "the nummer of command line arguments are passed $#"
 
-if [ $# -eq 2 ]; then 
-    echo "two arguments provided"
-else
-    echo "please provide two arguments"
-fi
+
+
+if [ $# -ne 0 ] then 
+    echo "
